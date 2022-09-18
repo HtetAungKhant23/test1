2f9a5060d26084e4076f939e880ce539063cf4c6
Htet Aung Khant <112607740+HtetAungKhant23@users.noreply.github.com>
2022-09-19 03:01:59 +0630

    Update test1.c
    1st commit

--- test1.c
+++ test1.c
@@ -6,8 +6,8 @@ typedef struct user
     int id;
     char name[20];
     char password[20];
-//    int age;
-//    char location[20];
+    int age;
+    char location[20];
 
 } user;
 
@@ -27,20 +27,18 @@ void addData()
     {
         printf("\nEnter User ID: ");
         scanf("%d",&u[i]);
-
-        fflush(stdin);
-
         printf("Enter User Name: ");
         scanf(" %[^\n]s",u[i].name);
         printf("Enter Password: ");
         scanf("%s",u[i].password);
-//       printf("Enter Age: ");
-//       scanf("%d",u[i].age);
 
-//       fflush(stdin);
+        int ag=0;
+        printf("Enter Age: ");
+        scanf("%d",&ag);
+        u[i].age = ag;
 
-//       printf("Enter Location: ");
-//       scanf(" %[^\n]s",u[i].location);
+        printf("Enter Location: ");
+        scanf(" %[^\n]s",u[i].location);
 
         fwrite(&u[i],sizeof(user),1,fptr);
     }
@@ -55,7 +53,7 @@ void display()
     fptr = fopen("file.txt","r");
     while(fread(&u1,sizeof(user),1,fptr))
     {
-        printf("\n%-5d%-20s%-20s",u1.id,u1.name,u1.password);
+        printf("\n%-5d%-20s%-8s%-5d%-10s",u1.id,u1.name,u1.password,u1.age,u1.location);
 
     }
     printf("\n");
@@ -69,21 +67,25 @@ void findData()
     FILE *fptr;
     int id, found;
     fptr = fopen("file.txt","r");
-    printf("\nEnter user ID to find: ");
-    scanf("%d",&id);
+
     while(fread(&u1,sizeof(user),1,fptr))
     {
+        printf("\nEnter user ID to find: ");
+        scanf("%d",&id);
+
         if(u1.id == id)
         {
             found=1;
-            printf("\n%-5d%-20s%-20s",u1.id,u1.name,u1.password);
+            printf("\n%-5d%-20s%-8s%-5d%-10s",u1.id,u1.name,u1.password,u1.age,u1.location);
         }
+
+        if(!found)
+        {
+            printf("\nUser not found");
+        }
+
     }
     printf("\n");
-    if(!found)
-    {
-        printf("\nUser not found\n");
-    }
 
     fclose(fptr);
 }
@@ -95,61 +97,63 @@ void updateData()
     int id, found;
     fptr = fopen("file.txt","r");
     fptr1 = fopen("update.txt","w");
-    printf("\nEnter user ID to update: ");
-    scanf("%d",&id);
+
     while(fread(&u1,sizeof(user),1,fptr))
     {
+        printf("\nEnter user ID to update: ");
+        scanf("%d",&id);
+
         if(u1.id == id)
         {
             found=1;
-            fflush(stdin);
-
             printf("Enter New User Name: ");
             scanf(" %[^\n]s",u1.name);
             printf("Enter New Password: ");
             scanf("%s",u1.password);
-            //       printf("Enter New Age: ");
-            //       scanf("%d",u1.age);
 
-            //       fflush(stdin);
+            int nage=0;
+            printf("Enter New Age: ");
+            scanf("%d",&nage);
+            u1.age = nage;
 
-            //       printf("Enter New Location: ");
-            //       scanf(" %[^\n]s",u1.location);
+            printf("Enter New Location: ");
+            scanf(" %[^\n]s",u1.location);
         }
         fwrite(&u1,sizeof(user),1,fptr1);
-    }
-    fclose(fptr);
-    fclose(fptr1);
-    if(found)
-    {
-        fptr1 = fopen("update.txt","r");
-        fptr = fopen("file.txt","w");
-
-        while(fread(&u1,sizeof(user),1,fptr1))
-        {
-            fwrite(&u1,sizeof(user),1,fptr);
-        }
-
 
         fclose(fptr);
         fclose(fptr1);
+        if(found)
+        {
+            fptr1 = fopen("update.txt","r");
+            fptr = fopen("file.txt","w");
 
+            while(fread(&u1,sizeof(user),1,fptr1))
+            {
+                fwrite(&u1,sizeof(user),1,fptr);
+            }
+
+            fclose(fptr);
+            fclose(fptr1);
+        }
+        else
+        {
+            printf("\nUser not found");
+        }
     }
-    else
-    {
-        printf("\nUser not found\n");
-    }
+
+    printf("\n");
+
 
 }
 
 
 int main()
 {
-
     int choice;
     do
     {
-        printf("\n1.ADD USER DATA\n");
+        printf("\n1.INSERT USER DATA\n");
         printf("2.DISPLAY USER DATA\n");
         printf("3.FIND USER DATA\n");
         printf("4.UPDATE USER DATA\n");
@@ -162,29 +166,28 @@ int main()
         {
         case 1:
             addData();
-            printf("------------------------------------------\n");
+            printf("-------------------------------------------------------------\n");
             break;
         case 2:
             display();
-            printf("------------------------------------------\n");
+            printf("-------------------------------------------------------------\n");
             break;
         case 3:
             findData();
-            printf("------------------------------------------\n");
+            printf("-------------------------------------------------------------\n");
             break;
         case 4:
             updateData();
-            printf("------------------------------------------\n");
-            break;
-        case 0;
-            printf("\nThanks for enjoy with us\n");
-            break;
-        default:
-            printf("\nInvalid Input\n");
+            printf("-------------------------------------------------------------\n");
             break;
         }
 
-
+        if(choice == 0)
+        {
+            printf("\n--------------------------------------------------\n");
+            printf("\t   Thanks for enjoy with us\n");
+            printf("--------------------------------------------------\n");
+        }
 
     }
     while(choice!=0);
@@ -192,3 +195,4 @@ int main()
 
     return 0;
 }
+
